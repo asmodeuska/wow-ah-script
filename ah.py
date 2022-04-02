7c7952babd3c66452726dea76ad73f2f50aa6d35
asmodeuska <szaboviktor161@gmail.com>
2022-04-02 21:20:39 +0200

    performance+

--- ah.py
+++ ah.py
@@ -5,7 +5,7 @@ import keyboard
 import threading
 import random
 
-#coordinates
+#coordinates 1920x1080
 mailBoxLocation = (0,0)
 postScan = (1229,525)
 cancelScan = (1203,555)
@@ -13,14 +13,30 @@ post = (1682,558)
 exit = (1133,92)
 cancel = (1642,555)
 openAllMail = (370,451)
+tsmImageSearchRegion = (1115,535, 476,42)
+ahErrorImageSerachRegion = (0,856,433,180)
+
+
+#coordinates 1366x768
+LmailBoxLocation = (0,0)
+LpostScan = (669,514)
+LcancelScan = (696,551)
+Lpost = (1127,546)
+Lexit = (602,78)
+Lcancel = (1135,549)
+LopenAllMail = (328,457)
+LtsmImageSearchRegion = (583,533, 468,39)
+LahErrorImageSerachRegion = (0,475,511,167)
 
 #keybinds
 relocateMailbox = ','
+startScript = '.'
 targetAH = 'num8'
 openAh = 'num7'
 
 #logic
 run = False
+highRes = True
 
 #delay
 ms = 0.1
@@ -45,7 +61,7 @@ def keyboard_input():
         if keyboard.is_pressed(relocateMailbox):
             setMailboxLocation()
             time.sleep(1)
-        if keyboard.is_pressed('.'):
+        if keyboard.is_pressed(startScript):
             run = not run
             time.sleep(1)
 
@@ -61,7 +77,7 @@ def openAH():
     time.sleep(ms)
     pyautogui.press('multiply')
     time.sleep(ms)
-    runPostScan()
+    runCancelScann()
 
 def runCancelScann():
     global interv
@@ -69,16 +85,18 @@ def runCancelScann():
     pyautogui.moveTo(cancelScan[0],cancelScan[1],random.uniform(interv-0.2,interv+0.2))
     pyautogui.click()
     time.sleep(random.uniform(2,3))
-    done_cancelling = pyautogui.locateOnScreen('done_cancelling.png',confidence=highConfidence)
+    done_cancelling = pyautogui.locateOnScreen('done_cancelling.png',confidence=highConfidence,region=tsmImageSearchRegion)
+    pyautogui.moveTo(cancel[0],cancel[1],random.uniform(interv-0.2,interv+0.2))
     while done_cancelling == None : 
-        pyautogui.moveTo(cancel[0],cancel[1],random.uniform(interv-0.2,interv+0.2))
+        if pyautogui.locateOnScreen('fail.png',confidence=mediumConfidence, region=ahErrorImageSerachRegion):
+            break
         pyautogui.click()
-        done_cancelling = pyautogui.locateOnScreen('done_cancelling.png',confidence=highConfidence)
+        done_cancelling = pyautogui.locateOnScreen('done_cancelling.png',confidence=highConfidence,region=tsmImageSearchRegion)
         time.sleep(ms)
     pyautogui.moveTo(exit[0],exit[1],random.uniform(interv-0.2,interv+0.2))
     pyautogui.click()
     time.sleep(random.uniform(1,2))
-    getMail()
+    runPostScan()
 
 def runPostScan():
     global interv
@@ -88,17 +106,17 @@ def runPostScan():
     pyautogui.moveTo(postScan[0],postScan[1],random.uniform(interv-0.2,interv+0.2))
     pyautogui.click()
     time.sleep(random.uniform(2,3))
-    done_posting = pyautogui.locateOnScreen('done_posting.png',confidence=highConfidence)
+    done_posting = pyautogui.locateOnScreen('done_posting.png',confidence=highConfidence,region=tsmImageSearchRegion)
     pyautogui.moveTo(post[0],post[1],random.uniform(interv-0.2,interv+0.2))
     while done_posting == None : 
         pyautogui.click()
-        done_posting = pyautogui.locateOnScreen('done_posting.png',confidence=highConfidence)
+        done_posting = pyautogui.locateOnScreen('done_posting.png',confidence=highConfidence,region=tsmImageSearchRegion)
         time.sleep(ms)
     pyautogui.moveTo(exit[0],exit[1],random.uniform(interv-0.2,interv+0.2))
     pyautogui.click()
     time.sleep(random.uniform(1,2))
     print('donePosting')
-    runCancelScann()
+    getMail()
 
 def getMail():
     global lag
@@ -122,6 +140,17 @@ def getMail():
     openAH()
 
 if __name__ == "__main__":
+    if highRes == False :
+        mailBoxLocation = LmailBoxLocation
+        postScan = LpostScan
+        cancelScan = LcancelScan
+        post = Lpost
+        exit = Lexit
+        cancel = Lcancel
+        openAllMail = LopenAllMail
+        tsmImageSearchRegion = LtsmImageSearchRegion
+        ahErrorImageSerachRegion = LahErrorImageSerachRegion
+
     threadin = threading.Thread(target=keyboard_input)
     threadin.daemon = True
     threadin.start()
